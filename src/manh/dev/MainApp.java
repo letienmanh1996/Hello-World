0aba6cbce9e5894f66bd3aff806ec936d6bcc378
unknown <letienmanhck11@gmail.com>
2022-08-23 18:54:30 +0700

    first commit

---
+++ src/manh/dev/MainApp.java
@@ -0,0 +1,39 @@
+package manh.dev;
+/**
+ * ghi chú nhiều dòng
+ * @author Lê Tiến Mạnh
+ */
+public class MainApp {
+	// ghi chú 1 dòng cách 1 phím nóng Ctr + /
+	/* ghi chú cách 2*/
+	public static void main(String[] args) {
+		/**
+		 * khai báo biến
+		 */
+		String firstName = "le tien";
+		String lastName ="Manh";
+		int old = 26;
+		float markMath =9f;
+		boolean passed = true;
+		System.out.println("Ho va ten");
+		System.out.println(firstName + " " + lastName);
+		System.out.println("Hello World");
+		System.out.println("Mark Math: " + passed);
+		// khai báo hằng, Hằng phải viết hoa/
+		final float PI =3.14f;
+		
+		/**
+		 * cách đặt tên biến
+		 */
+		int number;
+		String nameShool;
+	}
+	/**
+	 * tên phương thức tuân theo quy tắt camelcase
+	 */
+	public void show () {
+		
+	}
+}
+
+	
